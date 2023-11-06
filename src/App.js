c7c9a708af3bd7ccaebc02186ac736edf76c615c
Sejal-Pradhan <132825887+Sejal-Pradhan@users.noreply.github.com>
2023-11-06 13:16:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Homepage from "./components/homepage/Homepage";
 import { DefaultLayout } from './layout/DefaultLayout';
 import { Dashboard } from './pages/dashboard/Dashboard.page';
 
+import { LoginForm } from './components/login/Login.comp';
+import { ResetPassword } from './components/password-reset/PasswordReset.comp';
 import { Entry } from './pages/entry/Entry.page';
 import { AddTicket } from './pages/new-ticket/AddTicket.page';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Entry /> */}
-      <DefaultLayout>
-        {/* <Dashboard /> */}
-        <AddTicket />
-      </DefaultLayout>
+    <HashRouter>
+    <Routes>
+    <Route path="/" element={<Homepage />} />
+    <Route path="/client-login" element={<Entry />} />
+    <Route path="/forget-password" element={<ResetPassword />} />
+    </Routes>
+  </HashRouter>
     </div>
+
+  //   <div className="App">
+  //   { /* <Homepage /> */}
+  //     {/* <Entry /> */}
+  //      <DefaultLayout>
+      
+  //       <Dashboard />
+  //       <AddTicket />
+  // </DefaultLayout>
+  //   </div>
   );
 }
 
