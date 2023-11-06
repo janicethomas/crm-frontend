ff1ed678756909fe76135c2beab3e0e91a7c8f9e
Sejal-Pradhan <132825887+Sejal-Pradhan@users.noreply.github.com>
2023-11-06 16:24:07 +0530

    Add files via upload

---
+++ src/pages/entry/EntryAdmin.page.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+
+import { AdminLogin } from '../../components/login/Admin.login';
+
+import "./entry.style.css";
+import { ResetPassword } from '../../components/password-reset/PasswordReset.comp';
+
+export const EntryAdmin = () => {
+    const [email, setEmail] = useState('');
+    const [Password, setPassword] = useState('');
+    const [frmLoad, setFrmLoad] = useState('login');
+
+    const handleOnChange = e => {
+        const { name, value } = e.target
+
+        switch (name) {
+            case 'email':
+                setEmail(value)
+                break
+
+            case 'Password':
+                setPassword(value)
+                break
+
+            default:
+                break
+        }
+    };
+
+    const handleOnSubmit = e => {
+        e.preventDefault()
+        if (!email || !Password) {
+            return alert("Please enter both fields")
+        }
+        console.log(email, Password);
+    };
+
+    const handleOnResetSubmit = e => {
+        e.preventDefault()
+        if (!email) {
+            return alert("Please enter email")
+        }
+        console.log(email);
+    };
+
+    const formSwitcher = (frmType) => {
+        setFrmLoad(frmType);
+    };
+
+    return (
+        <div className='entry-page bg-info'>
+            <div className='jumbotron'>
+                {frmLoad === 'login' && <AdminLogin
+                    handleOnChange={handleOnChange}
+                    handleOnSubmit={handleOnSubmit}
+                    formSwitcher={formSwitcher}
+                    email={email}
+                    pass={Password}
+                />}
+                {frmLoad === 'reset' && <ResetPassword
+                    handleOnChange={handleOnChange}
+                    handleOnResetSubmit={handleOnResetSubmit}
+                    formSwitcher={formSwitcher}
+                    email={email}
+                />}
+            </div>
+        </div>
+    )
+}
