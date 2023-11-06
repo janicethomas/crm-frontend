9033d9d2781325f6149cef8f146b58f42e658b1e
Sejal-Pradhan <132825887+Sejal-Pradhan@users.noreply.github.com>
2023-11-06 13:18:17 +0530

    Update Login.comp.js

--- src/components/login/Login.comp.js
+++ src/components/login/Login.comp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 export const LoginForm = ({ handleOnChange, handleOnSubmit, formSwitcher, email, pass }) => {
