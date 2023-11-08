768c2939d59bce8a0a9cc935af1217004e6fe207
Sejal-Pradhan <132825887+Sejal-Pradhan@users.noreply.github.com>
2023-11-08 19:10:26 +0530

    Update EntryAdmin.page.js

--- src/pages/entry/EntryAdmin.page.js
+++ src/pages/entry/EntryAdmin.page.js
@@ -1,69 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { AdminLogin } from '../../components/login/Admin.login';
+import { AgentLogin } from "../../components/login/Agent.login";
 
 import "./entry.style.css";
-import { ResetPassword } from '../../components/password-reset/PasswordReset.comp';
+import { ResetPassword } from "../../components/password-reset/PasswordReset.comp";
 
-export const EntryAdmin = () => {
-    const [email, setEmail] = useState('');
-    const [Password, setPassword] = useState('');
-    const [frmLoad, setFrmLoad] = useState('login');
+export const EntryAgent = () => {
+  const [email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [frmLoad, setFrmLoad] = useState("login");
 
-    const handleOnChange = e => {
-        const { name, value } = e.target
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
 
-        switch (name) {
-            case 'email':
-                setEmail(value)
-                break
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
 
-            case 'Password':
-                setPassword(value)
-                break
+      case "Password":
+        setPassword(value);
+        break;
 
-            default:
-                break
-        }
-    };
+      default:
+        break;
+    }
+  };
 
-    const handleOnSubmit = e => {
-        e.preventDefault()
-        if (!email || !Password) {
-            return alert("Please enter both fields")
-        }
-        console.log(email, Password);
-    };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !Password) {
+      return alert("Please enter both fields");
+    }
+    console.log(email, Password);
+  };
 
-    const handleOnResetSubmit = e => {
-        e.preventDefault()
-        if (!email) {
-            return alert("Please enter email")
-        }
-        console.log(email);
-    };
+  const handleOnResetSubmit = (e) => {
+    e.preventDefault();
+    if (!email) {
+      return alert("Please enter email");
+    }
+    console.log(email);
+  };
 
-    const formSwitcher = (frmType) => {
-        setFrmLoad(frmType);
-    };
+  const formSwitcher = (frmType) => {
+    setFrmLoad(frmType);
+  };
 
-    return (
-        <div className='entry-page bg-info'>
-            <div className='jumbotron'>
-                {frmLoad === 'login' && <AdminLogin
-                    handleOnChange={handleOnChange}
-                    handleOnSubmit={handleOnSubmit}
-                    formSwitcher={formSwitcher}
-                    email={email}
-                    pass={Password}
-                />}
-                {frmLoad === 'reset' && <ResetPassword
-                    handleOnChange={handleOnChange}
-                    handleOnResetSubmit={handleOnResetSubmit}
-                    formSwitcher={formSwitcher}
-                    email={email}
-                />}
-            </div>
+  return (
+    <div>
+      <div className="entry-page bg-info">
+        <div className="jumbotron">
+          {frmLoad === "login" && (
+            <AgentLogin
+              handleOnChange={handleOnChange}
+              handleOnSubmit={handleOnSubmit}
+              formSwitcher={formSwitcher}
+              email={email}
+              pass={Password}
+            />
+          )}
+          {frmLoad === "reset" && (
+            <ResetPassword
+              handleOnChange={handleOnChange}
+              handleOnResetSubmit={handleOnResetSubmit}
+              formSwitcher={formSwitcher}
+              email={email}
+            />
+          )}
         </div>
-    )
-}
+      </div>
+      <div class="footer-bottom">
+        <div class="container">
+          <p class="m-0 text-center">
+            &copy; 2023 <i>CRM360</i>. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
