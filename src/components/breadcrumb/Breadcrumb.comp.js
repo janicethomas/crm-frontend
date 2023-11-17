48b9353acb081248013798be836b624f2cdbc4f7
Sejal-Pradhan <132825887+Sejal-Pradhan@users.noreply.github.com>
2023-11-17 21:51:11 +0530

    Update Breadcrumb.comp.js

--- src/components/breadcrumb/Breadcrumb.comp.js
+++ src/components/breadcrumb/Breadcrumb.comp.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Breadcrumb } from "react-bootstrap";
 // import { LinkContainer } from "react-router-bootstrap";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import "./breadcrumb.css";
 
 export const PageBreadcrumb = ({ page }) => {
     return (
         <Breadcrumb>
             {/* <LinkContainer to="/"> */}
             <Breadcrumb.Item>
-            <Link to="/dashboard">
-                        Home
+            <Link to="/">
+                        <span className="bread">Home/</span>
                     </Link>
             </Breadcrumb.Item>
             {/* </LinkContainer> */}
-            <Breadcrumb.Item active>{page}</Breadcrumb.Item>
+            <Breadcrumb.Item active><span className="bread1">{page}</span></Breadcrumb.Item>
         </Breadcrumb>
     );
 };
